5b9b8c98755670ab4b8ed3723938c469ea92cc46
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-08-10 22:28:25 -0700

    shell test

--- shell/main.c
+++ shell/main.c
@@ -10,7 +10,7 @@
 int main(int c, char **env)
 {
 	(void)c;
-	char *prompt = "(Simple Shell)$ ", *buffer = NULL, *path;
+	char *prompt = "(Simple_Shell)$ ", *buffer = NULL, *path;
 	size_t buffsize = 0;
 	char *arg[11], *delim = "\n";
 	int status, a, b, exe;
@@ -22,7 +22,7 @@ int main(int c, char **env)
 		if(isatty(0))
 			_printstring(prompt);
 
-		n_char = getline(&buffer, &buffsize, stdin);
+		n_chars = getline(&buffer, &buffsize, stdin);
 
 		if (n_chars == -1)
 		{
@@ -30,5 +30,50 @@ int main(int c, char **env)
 			exit(0);
 		}
 
-		a = 0
-			while
+		a = 0;
+			while (buffer[a])
+			{
+				if (buffer[a] == '\n')
+					buffer[a] = 0;
+				a++;
+			}
+		b = 0;
+		arg[b] = strtok(buffer, delim);
+		while (arg[b])
+		{
+			arg[++b] = strtok(NULL, delim);
+		}
+		arg[b] = NULL;
+
+		path = get_loc(arg[0]);
+		if (path == NULL)
+		{
+			if (_builtInCmd(arg) != 0)
+			{
+				continue;
+			}
+			else
+				_printstring("Comand not found\n");
+			continue;
+		}
+
+		Child_ID = fork();
+		if (Child_ID < 0)
+		{
+			_printstring("Forking failed");
+			free(buffer);
+			exit(0);
+		}
+		else if (Child_ID == 0)
+		{
+			exe = execve(path, arg, env);
+			if (exe == -1)
+				_printstring("Command does not exist\n");
+		}
+		else
+			wait(&status);
+	}
+	free(path);
+	free(buffer);
+	return (0);
+}
