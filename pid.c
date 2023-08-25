e1e7e885f2838776d0f182d6e97f7341da8d795b
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-08-24 18:52:45 -0700

    update

--- pid.c
+++ pid.c
@@ -3,13 +3,26 @@
 
 int main(void)
 {
-	pid_t pid, ppid;
+	/* declaration of pid */
+	pid_t pid, ppid, c_pid;
 
+	/*initialising*/
 	pid = getpid();
 	ppid = getppid();
 
 	printf("The process ID is:%d\n", pid);
-	printf("The parent process ID is: %d\n", ppid);
+	printf("Tme parent process ID is: %d\n", ppid);
+	c_pid = fork();
+	if (c_pid ==0)
+	{
+		printf("This is a child process\n");
+		printf("Parent process ID (PPID): %d\n", getppid());
+		printf("current process ID (PID): %d\n", getpid());
+	}
+	else if (c_pid > 0)
+	{
+		printf(This is a parent process:\n");
+		printf
 
 	return (0);
 }
--- shell/main.c
+++ shell/main.c
@@ -10,62 +10,47 @@
 int main(int c, char **env)
 {
 	(void)c;
-	char *prompt = "(Simple_Shell)$ ", *buffer = NULL, *path;
+	char *prompt = "(Simple_Shell)$ ", *buffer = NULL, *path, *arg[11], *delim = " \n";
 	size_t buffsize = 0;
-	char *arg[11], *delim = "\n";
 	int status, a, b, exe;
 	ssize_t n_chars;
 	pid_t Child_ID;
 
-	while (1)
-	{
+	while (1) {
 		if(isatty(0))
-			_printstring(prompt);
-
+		_printstring(prompt);
 		n_chars = getline(&buffer, &buffsize, stdin);
-
-		if (n_chars == -1)
-		{
+		if (n_chars == -1) {
 			free(buffer);
 			exit(0);
 		}
-
 		a = 0;
-			while (buffer[a])
-			{
+			while (buffer[a]) {
 				if (buffer[a] == '\n')
 					buffer[a] = 0;
 				a++;
 			}
 		b = 0;
 		arg[b] = strtok(buffer, delim);
-		while (arg[b])
-		{
+		while (arg[b]) {
 			arg[++b] = strtok(NULL, delim);
 		}
 		arg[b] = NULL;
-
 		path = get_loc(arg[0]);
-		if (path == NULL)
-		{
+		if (path == NULL) {
 			if (_builtInCmd(arg) != 0)
-			{
 				continue;
-			}
 			else
 				_printstring("Comand not found\n");
 			continue;
 		}
-
 		Child_ID = fork();
-		if (Child_ID < 0)
-		{
+		if (Child_ID < 0) {
 			_printstring("Forking failed");
 			free(buffer);
 			exit(0);
 		}
-		else if (Child_ID == 0)
-		{
+		else if (Child_ID == 0) {
 			exe = execve(path, arg, env);
 			if (exe == -1)
 				_printstring("Command does not exist\n");
