e78df0e20deee7d125e5708257dc4fcb6e57a1f2
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-09-18 06:19:34 -0600

    update

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -14,8 +14,14 @@ int _putchar(char c)
 void print_number(int n)
 {
     if (n < 0)
-    n = -n;
-    _putchar('0' + (n / 10));
+    {
+        _putchar('-');
+        n = -n;
+    }
+    if (n / 10)
+    {
+        print_number(n / 10);
+    }
     _putchar('0' + (n % 10));
 }
 int main(void)
